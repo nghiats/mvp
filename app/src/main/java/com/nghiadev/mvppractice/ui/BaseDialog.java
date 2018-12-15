0753958c18f23ffe094275299819dd388b3f8e7f
nghiatran <nghia.tsy@gmail.com>
2018-12-15 11:21:39 +0700

    Setup BaseDialog

---
+++ app/src/main/java/com/nghiadev/mvppractice/ui/BaseDialog.java
@@ -0,0 +1,112 @@
+package com.nghiadev.mvppractice.ui;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.RelativeLayout;
+
+import butterknife.Unbinder;
+
+public abstract class BaseDialog extends DialogFragment implements DialogMvpView {
+    private BaseActivity mActivity;
+    private Unbinder mUnBinder;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof BaseActivity) {
+            BaseActivity activity = (BaseActivity) context;
+            mActivity = activity;
+            activity.onFragmentAttached();
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        mActivity.onFragmentDetached("");
+        super.onDetach();
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        mActivity.onFragmentDetached("");
+        super.onDismiss(dialog);
+    }
+
+    public void setUnBinder(Unbinder mUnBinder) {
+        this.mUnBinder = mUnBinder;
+    }
+
+    protected abstract void setUp(View view);
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // the content
+        final RelativeLayout root = new RelativeLayout(getActivity());
+        root.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        // creating the fullscreen dialog
+        final Dialog dialog = new Dialog(getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(root);
+        if (dialog.getWindow() != null) {
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().setLayout(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+        dialog.setCanceledOnTouchOutside(false);
+
+        return dialog;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setUp(view);
+    }
+
+    public void show(FragmentManager fragmentManager, String tag) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        Fragment prevFragment = fragmentManager.findFragmentByTag(tag);
+        if (prevFragment != null) {
+            transaction.remove(prevFragment);
+        }
+        transaction.addToBackStack(null);
+        show(transaction, tag);
+    }
+
+    @Override
+    public void dismissDialog(String tag) {
+        dismiss();
+        getBaseActivity().onFragmentDetached(tag);
+    }
+
+    private BaseActivity getBaseActivity() {
+        return mActivity;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mUnBinder != null) {
+            mUnBinder.unbind();
+        }
+        super.onDestroy();
+    }
+
+}
